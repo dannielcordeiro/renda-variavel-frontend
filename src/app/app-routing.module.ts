3e137853ab3548147a3fc9fb5231db15c76d5c2c
dannielcordeiro <dannielcordeiro@hotmail.com>
2020-07-31 12:38:40 -0300

    Rota e formulário adicionados

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { NotaCorretagemComponent } from 'src/app/nota-corretagem/nota-corretagem.component'
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'notas', component: NotaCorretagemComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
